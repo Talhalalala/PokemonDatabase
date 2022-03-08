ec9458e10eb03098181d3d958ce1f84f6074773a
“talhalalala” <talhasadak@outlook.comgit config --global user.name Talhalalala>
2022-03-08 16:00:18 +0000

    yarn seed added for forest production

--- app/src/components/atoms/Test/Test.tsx
+++ app/src/components/atoms/Test/Test.tsx
@@ -1,7 +1,10 @@
 import React, { FC } from 'react';
 import {gql} from 'graphql-tag';
+import { QueryClient, QueryClientProvider } from 'react-query'
 import { useExampleQueryQuery } from '../../../graphql/generated';
 
+
+const queryClient = new QueryClient();
 
 gql`query ExampleQuery {
     findManyPokemon {
@@ -21,5 +24,11 @@ export const Test: FC<Props> = (props) => {
     const { data } = useExampleQueryQuery();
 
     return <div>{data?.findManyPokemon?.[0].name}</div>;
+    // <div>
+    //     <QueryClientProvider client={queryClient}>
+    //         <div>{data?.findManyPokemon?.[0].name}</div>
+    //     </QueryClientProvider>;
+    // </div>
+    // return <div><h1>Test</h1></div>)
 }
     
