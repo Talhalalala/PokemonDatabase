7d3f15372711819cdb39a6e35a6d5f66e257b0cb
TalhaVault <tahla.sadak@vaultplatform.com>
2022-04-13 13:53:45 +0100

    clean up

--- app/src/components/atoms/TableData/TableData.tsx
+++
@@ -1,9 +0,0 @@
-import React, { FC } from 'react';
-
-type Props = {
-    heading: string
-}
-
-export const TableData: FC<Props> = ({ children, heading }) => {
-    return <p>{heading}: {children}</p>
-};
--- app/src/components/atoms/TableRow/TableRow.tsx
+++
@@ -1,8 +0,0 @@
-import React, { FC } from 'react';
-
-type Props = {
-}
-
-export const TableRow: FC<Props> = ({children}) => {
-    return <div>{children}</div>
-};
